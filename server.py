e17a8da3904839e3e9cdf61b7afea243889cf21b
Erick Vaz <137460493+ErickVzM@users.noreply.github.com>
2025-09-12 16:40:00 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,50 @@
 from socket  import *
-from constCS import * #-
+from constCS import *
 import pickle
 
 s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
-s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client 
-while True:                # forever
-  msg = conn.recv(1024)    # receive data from client
-  if not msg: break        # stop if client stopped 
-  print(msg)
-  data = pickle.loads(msg)
-  print(data)
-  if data["OP"] == "sum":
-    res = data["V1"] + data["V2"]
-    status = "OK"
-  elif data["OP"] == "sub":
-    res = data["V1"] - data["V2"]
-    status = "OK"
-  else:
-    status = "NOK"
-    res = 1
-  data = {"STATUS":status, "RES":res}
-  msg = pickle.dumps(data)
-  conn.send(msg)
+s.bind((HOST, PORT))
+s.listen(1)
+(conn, addr) = s.accept()
+print(f"Conectado a {addr}")
+
+while True:
+  msg = conn.recv(1024)
+  if not msg:
+    break
   
-conn.close()               # close the connection
+  try:
+    data = pickle.loads(msg)
+    
+    if "OP" in data and "STR" in data:
+      op = data["OP"]
+      string_recebida = data["STR"]
+      
+      if op == "upper":
+        res = string_recebida.upper()
+        status = "OK"
+      elif op == "lower":
+        res = string_recebida.lower()
+        status = "OK"
+      elif op == "invert":
+        res = string_recebida[::-1]
+        status = "OK"
+      else:
+        status = "NOK"
+        res = "Operação inválida"
+    else:
+      status = "NOK"
+      res = "Formato de dados inválido."
+      
+    data_resposta = {"STATUS": status, "RES": res}
+    msg_resposta = pickle.dumps(data_resposta)
+    conn.send(msg_resposta)
+    
+  except (pickle.UnpicklingError, IndexError):
+    status = "NOK"
+    res = "Erro ao processar a mensagem."
+    data_resposta = {"STATUS": status, "RES": res}
+    msg_resposta = pickle.dumps(data_resposta)
+    conn.send(msg_resposta)
+
+conn.close()         # close the connection
